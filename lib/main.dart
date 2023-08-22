c058318023a5bd599ba2ee7cd34740300ff7d0b6
Voodster <voods.work@yandex.ru>
2023-08-22 17:03:23 +0300

    simple fix

--- lib/main.dart
+++ lib/main.dart
@@ -86,7 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        actions: [IconButton(onPressed: actoinButton(context), icon: Icon(Icons.add)),IconButton(onPressed: actoinButton(), icon: Icon(Icons.add)),IconButton(onPressed: actoinButton(), icon: Icon(Icons.add)),IconButton(onPressed: actoinButton(), icon: Icon(Icons.add))],
+        actions: [IconButton(onPressed: actoinButton(context), icon: Icon(Icons.add)),IconButton(onPressed: actoinButton(context), icon: Icon(Icons.add)),IconButton(onPressed: actoinButton(context), icon: Icon(Icons.add)),IconButton(onPressed: actoinButton(context), icon: Icon(Icons.add))],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
