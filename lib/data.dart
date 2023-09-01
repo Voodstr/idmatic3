58530868c715743178af05766f2c714fd662834e
Voodstr <voods.work@yandex.ru>
2023-09-01 13:24:58 +0300

    some minor fixes

--- lib/data.dart
+++ lib/data.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+//TODO создать структуру базы данных
+
 class AccessLevel {
   int id = 0;
   String name = "default access level";
@@ -21,6 +23,10 @@ class TimeZone {
   TimeOfDay endTime = const TimeOfDay(hour: 0, minute: 0);
 }
 
+//TODO метод формирования инт из двоичного
+
+
+
 List<bool> privilegesToList(int privilegeInt,int size){
   List<bool> privilegesToList = [];
   for (int i = 0; i < (size - privilegeInt.toRadixString(2).length); i++) {
@@ -33,5 +39,6 @@ List<bool> privilegesToList(int privilegeInt,int size){
       privilegesToList.add(false);
     }
   });
+  var data = int.parse("source",radix: 2);
   return privilegesToList;
 }
